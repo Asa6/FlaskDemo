cf9d1e8b8bdfed74eed210a6db117eb40f6a9768
Asa6 <18518353480@163.com>
2020-06-05 17:15:11 +0800

    1. 实现基本的http GET/POST请求

--- flaskr/views/__init__.py
+++ flaskr/views/__init__.py
@@ -2,6 +2,6 @@
 #     Blueprint, flash, g, redirect, render_template, request, url_for
 # )
 
-from flask import Blueprint
-bp = Blueprint('main', __name__)
+# from flask import Blueprint
+# bp = Blueprint('main', __name__)
 
--- flaskr/views/cmdb.py
+++ flaskr/views/cmdb.py
@@ -1,7 +1,30 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, request
+)
 bp = Blueprint('cmdb', __name__)
 
 
-@bp.route('/api/v1/cmdbs')
+@bp.route('/api/v1/cmdbs', methods=['GET', 'POST'])
 def index():
-    return 'Hello, cmdb!'
+
+    print(request.method)
+
+    # 获取GET请求方法
+    if request.method == 'GET':
+
+        # 获取get请求参数：ip
+        ip = request.args.get('ip')
+        print('ip:', ip)
+
+        return 'CMDB, GET!'
+
+
+    # 获取POST请求方法
+    if request.method == 'POST':
+
+        # 获取body内容
+        post_body = request.form
+        print('post_body:', post_body)
+
+
+        return 'CMDB, POST!'
